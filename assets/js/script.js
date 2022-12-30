81be211f7507a6c6d4153a980f849d3819c83348
pandaicoding <pandaicoding@gmail.com>
2022-12-30 17:35:01 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,220 @@
+"use strict";
+
+/**
+ * Add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+/**
+ * MOBILE NAVBAR TOGGLER
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+/**
+ * HEADER ANIMATION
+ * When scrolled donw to 100px header will be active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+/**
+ * SLIDER
+ */
+
+const slider = document.querySelector("[data-slider]");
+const sliderContainer = document.querySelector("[data-slider-container]");
+const sliderPrevBtn = document.querySelector("[data-slider-prev]");
+const sliderNextBtn = document.querySelector("[data-slider-next]");
+
+let totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+let currentSlidePos = 0;
+
+const moveSliderItem = function () {
+  sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+};
+
+/**
+ * NEXT SLIDE
+ */
+
+const slideNext = function () {
+  const slideEnd = currentSlidePos >= totalSlidableItems;
+
+  if (slideEnd) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+
+  moveSliderItem();
+};
+
+sliderNextBtn.addEventListener("click", slideNext);
+
+/**
+ * PREVIOUS SLIDE
+ */
+
+const slidePrev = function () {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = totalSlidableItems;
+  } else {
+    currentSlidePos--;
+  }
+
+  moveSliderItem();
+};
+
+sliderPrevBtn.addEventListener("click", slidePrev);
+
+/**
+ * RESPONSIVE
+ */
+window.addEventListener("resize", function () {
+  totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+  totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  moveSliderItem();
+});
+
+/**
+ * TEXT ANIMATION EFFECT FOR HERO SECTION
+ */
+
+const letterBoxes = document.querySelectorAll("[data-letter-effect]");
+
+let activeLetterBoxIndex = 0;
+let lastActiveLetterBoxIndex = 0;
+let totalLetterBoxDelay = 0;
+
+const setLetterEffect = function () {
+  // loop through all letter boxes
+  for (let i = 0; i < letterBoxes.length; i++) {
+    // set initial animation delay
+    let letterAnimationDelay = 0;
+
+    // get all character from the current letter box
+    const letters = letterBoxes[i].textContent.trim();
+    // remove all character from the current letter box
+    letterBoxes[i].textContent = "";
+
+    // loop through all letters
+    for (let j = 0; j < letters.length; j++) {
+      // create a span
+      const span = document.createElement("span");
+
+      // set animation delay on span
+      span.style.animationDelay = `${letterAnimationDelay}s`;
+
+      // set the "in" class on the span, if current letter box is active
+      // otherwise class is "out"
+      if (i === activeLetterBoxIndex) {
+        span.classList.add("in");
+      } else {
+        span.classList.add("out");
+      }
+
+      // pass current letter into span
+      span.textContent = letters[j];
+
+      // add space class on span, when current letter contain space
+      if (letters[j] === " ") span.classList.add("space");
+
+      // pass the span on current letter box
+      letterBoxes[i].appendChild(span);
+
+      // skip letterAnimationDelay when loop is in the last index
+      if (j >= letters.length - 1) break;
+      // otherwise update
+      letterAnimationDelay += 0.05;
+    }
+
+    // get total delay of active letter box
+    if (i === activeLetterBoxIndex) {
+      totalLetterBoxDelay = Number(letterAnimationDelay.toFixed(2));
+    }
+
+    // add active class on last active letter box
+    if (i === lastActiveLetterBoxIndex) {
+      letterBoxes[i].classList.add("active");
+    } else {
+      letterBoxes[i].classList.remove("active");
+    }
+  }
+
+  setTimeout(function () {
+    lastActiveLetterBoxIndex = activeLetterBoxIndex;
+
+    // update activeLetterBoxIndex based on total letter boxes
+    activeLetterBoxIndex >= letterBoxes.length - 1 ? (activeLetterBoxIndex = 0) : activeLetterBoxIndex++;
+
+    setLetterEffect();
+  }, totalLetterBoxDelay * 1000 + 3000);
+};
+
+// call the letter effect function after window loaded
+window.addEventListener("load", setLetterEffect);
+
+const toggle = document.querySelector("#toggle");
+
+toggle.addEventListener("click", modeSwitch);
+
+let isLight = true;
+
+function modeSwitch() {
+  isLight = !isLight;
+  let root = document.body;
+  let header = document.querySelector("header");
+  let sectionTitle = document.getElementsByClassName("section-title")[0];
+  let emailSub = document.getElementsByClassName("input-wrapper")[0];
+  let topicCard = document.getElementsByClassName("topic-card")[0];
+
+  console.log(topicCard);
+
+  isLight ? (toggle.innerText = "🌞") : (toggle.innerText = "🌚");
+
+  root.classList.toggle("cover_light");
+
+  // header
+  header.classList.toggle("dark");
+  header.classList.toggle("light");
+
+  // section title
+  sectionTitle.classList.toggle("dark");
+  sectionTitle.classList.toggle("light");
+
+  // email subsection
+  emailSub.classList.toggle("dark");
+  emailSub.classList.toggle("light");
+
+  // topic card
+  topicCard.classList.toggle("dark");
+  topicCard.classList.toggle("light");
+}
